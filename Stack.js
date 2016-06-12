69eb75375b119d46e9d0cb27436f17b57c04e111
zhangsiyuan <1021808625@qq.com>
2016-06-12 15:46:25 +0800

    init~ Stack.js

---
+++ Stack.js
@@ -0,0 +1,33 @@
+/**
+ * Created by zhangsiyuan on 16/6/12.
+ */
+function Stack() {
+  var item = []
+  this.push = function (el) {
+    item.push(el)
+  }
+
+  this.pop = function () {
+    return item.pop()
+  }
+
+  this.peek = function () {
+    return item[item.length - 1]
+  }
+  
+  this.isEmpty = function () {
+    return !item.length
+  }
+
+  this.size = function () {
+    return item.length
+  }
+
+  this.clear = function () {
+    item = []
+  }
+
+  this.print = function () {
+    console.log(item.toString())
+  }
+}

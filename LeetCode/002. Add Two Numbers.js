97352e0c194c69f750c9008a08400f3ae801d207
zhangsiyuan <zhangsiyuan@baidu.com>
2018-02-22 18:06:36 +0800

    update: 002

--- LeetCode/002. Add Two Numbers.js
+++ LeetCode/002. Add Two Numbers.js
@@ -4,6 +4,8 @@
  *     this.val = val;
  *     this.next = null;
  * }
+ *
+ * 思路：请考虑进位carry；
  */
 /**
  * @param {ListNode} l1
@@ -11,19 +13,20 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-  var root = new ListNode(-1)
-  var cur = root
-  var carry = 0
-  var sum = 0
-  while (l1 !== null || l2 !== null || carry === 1) {
-    sum = (l1 === null ? 0 : l1.val) + (l2 === null ? 0 : l2.val) + carry
-    var node = new ListNode(sum % 10)
-    carry = Math.floor(sum / 10)
-    cur.next = node
-    cur = node
-    l1 = (l1 !== null ? l1.next : null);
-    l2 = (l2 !== null ? l2.next : null);
-  }
+    const root = new ListNode(-1);
+    let cur = root;
+    let carry = 0;
+    let sum = 0;
 
-  return root.next
-}
+    while (l1 || l2 || carry) {
+        sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+        carry = Math.floor(sum / 10);
+        cur.next = new ListNode(sum % 10);
+
+        cur = cur.next;
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
+
+    }
+    return root.next;
+};
